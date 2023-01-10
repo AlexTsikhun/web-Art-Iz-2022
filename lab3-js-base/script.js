2637a0c015396dac32baeb4184ee53e6f73c12fc
Alexander <sasacihun4@gmail.com>
2023-01-10 14:28:44 +0200

    Second task, ascii

--- lab3-js-base/script.js
+++ lab3-js-base/script.js
@@ -1,10 +1,34 @@
-var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+// program that doesn't like long names)
+console.log(`Base method:`);
+var names_base = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
-for (let i = 0; i < names.length; i++) {
-// console.log(names[i][0]);
-  if (names[i][0]=="J") {
-    goodBye_greeting.speak(names[i]);
-  } else {
-    hello_greeting.speak(names[i]);
-  }
+for (let i = 0; i < names_base.length; i++) {
+
+    if (names_base[i].charAt(0).toLocaleLowerCase()==="j") { // or names[i][0]
+        goodBye_greeting.speak(names_base[i]);
+    } else {
+        hello_greeting.speak(names_base[i]);
+    }
+}
+/////////////////////////////////////////////////
+console.log(`//////////////////
+ASCII method
+(count for eatch word ascii code):`);
+
+var names_ascii = ["Finnegan", "Antonio", "Jen", "Jason", "Angelina", "Frank", "Steven", "Larry", "Paula", "Maximilian", "Yan", "David", "Erling", "Paul"];
+
+
+let sum_ascii = 0;
+for (let i = 0; i<names_ascii.length; i++){
+    for (let j=0; j<names_ascii[i].length; j++){
+        let count_ascii_names = names_ascii[i].charCodeAt(j)
+        sum_ascii+=count_ascii_names;
+    }
+    // console.log(sum_ascii); //can watch scii value
+    if (sum_ascii>700) {
+        goodBye_greeting.speak(names_ascii[i]);
+    } else {
+        hello_greeting.speak(names_ascii[i]);
+    }
+    sum_ascii=0;
 }
